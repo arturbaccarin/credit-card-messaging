a70962239a72c04e6e4865250ca3caadcedf8e47
arturbaccarin <baccarin.artur@gmail.com>
2024-01-21 13:41:19 -0300

    perf: :zap: change receive method to Get function

--- rabbitmq-service/pkg/rabbitmq/receive.go
+++ rabbitmq-service/pkg/rabbitmq/receive.go
@@ -1,51 +1,20 @@
 package rabbitmq
 
 import (
-	"log"
-
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func Consume(ch *amqp.Channel) ([]byte, error) {
+	defer ch.Close()
 	var messages []byte
 
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	go func() {
-		for {
-			msg, ok := <-msgs
-			if !ok {
-				break
-			}
-			log.Printf(" > Received message: %s\n", msg.Body)
+	for {
+		msg, ok, _ := ch.Get("hello", true)
+		if !ok {
+			break
 		}
-	}()
-
-	ch.Close()
+		messages = append(messages, msg.Body...)
+	}
 
 	return messages, nil
 }

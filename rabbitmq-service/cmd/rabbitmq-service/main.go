c61d02f0ad680be79ae1d45df2d99579fb8abbba
arturbaccarin <baccarin.artur@gmail.com>
2024-02-12 11:20:21 -0300

    feat: :wrench: add initial rabbit configs

--- rabbitmq-service/cmd/rabbitmq-service/main.go
+++ rabbitmq-service/cmd/rabbitmq-service/main.go
@@ -3,22 +3,33 @@ package main
 import (
 	"log"
 
+	"github.com/arturbaccarin/credit-card-messaging/rabbitmq-service/internal/config"
 	"github.com/arturbaccarin/credit-card-messaging/rabbitmq-service/internal/webserver/handler"
 	"github.com/arturbaccarin/credit-card-messaging/rabbitmq-service/pkg/rabbitmq"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	ampq "github.com/rabbitmq/amqp091-go"
 )
 
-func main() {
-	conn := rabbitmq.NewConnection()
+var conn *ampq.Connection
+var ch *ampq.Channel
 
+func init() {
+	conn = rabbitmq.NewConnection()
+	ch = rabbitmq.NewChannel(conn)
+	rabbitmq.NewDirectExchange(ch, "exchange")
+	config.NewQueue(ch, "register", "exchange", []string{"r", "a"})
+	config.NewQueue(ch, "audit", "exchange", []string{"a"})
+}
+
+func main() {
 	app := fiber.New()
 	app.Use(
 		logger.New(),
 	)
 
-	sendHandler := handler.NewSendHandler(conn)
-	receiveHandler := handler.NewReceiveHandler(conn)
+	sendHandler := handler.NewSendHandler(ch)
+	receiveHandler := handler.NewReceiveHandler(ch)
 
 	app.Post("/send", sendHandler.SendMessage)
 	app.Get("/receive", receiveHandler.ReceiveMessages)

24e2efa0ae187b43b434e0df6207a1fe130fc8f7
arturbaccarin <baccarin.artur@gmail.com>
2024-02-13 11:35:20 -0300

    refactor: :recycle: adjust main in functions

--- payment-service/cmd/payment-service/main.go
+++ payment-service/cmd/payment-service/main.go
@@ -15,28 +15,53 @@ func main() {
 
 	paymentOrders := paymentOrderGenerator.GenerateCopies()
 
+	sendindOrders(paymentOrders)
+}
+
+func sendindOrders(paymentOrders []model.PaymentOrder) {
 	for _, paymentOrder := range paymentOrders {
-		message, err := json.Marshal(paymentOrder)
+		payload, err := parseMessage(paymentOrder)
 		if err != nil {
-			fmt.Println("Error marshalling struct to JSON:", err)
 			continue
 		}
 
-		body := model.RabbitMQPayload{
-			Message: string(message),
-		}
+		sendMessage(paymentOrder.NeedAudit, payload)
+	}
+}
 
-		payload, err := json.Marshal(body)
-		if err != nil {
-			fmt.Println("Error marshalling struct to JSON:", err)
-			continue
-		}
+func parseMessage(paymentOrder model.PaymentOrder) ([]byte, error) {
+	message, err := json.Marshal(paymentOrder)
+	if err != nil {
+		fmt.Println("Error marshalling struct to JSON:", err)
+		return nil, err
+	}
 
-		_, err = http.Post("http://localhost:3000/send", "application/json", bytes.NewBuffer(payload))
-		if err != nil {
-			fmt.Println("Error sending message:", err)
-		}
+	body := model.RabbitMQPayload{
+		Message: string(message),
+	}
 
-		fmt.Println("Message sent successfully")
+	payload, err := json.Marshal(body)
+	if err != nil {
+		fmt.Println("Error marshalling struct to JSON:", err)
+		return nil, err
+	}
+
+	return payload, nil
+}
+
+func sendMessage(needAudit bool, payload []byte) {
+	var key string
+
+	if needAudit {
+		key = "a"
+	} else {
+		key = "r"
+	}
+
+	url := fmt.Sprintf("http://localhost:3000/send?k=%s", key)
+
+	_, err := http.Post(url, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		fmt.Println("Error sending message:", err)
 	}
 }
